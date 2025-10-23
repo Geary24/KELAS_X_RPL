f930437a0f7783e03d82bd323b48c5cfc5bb2d2b
Geary24 <gearyeramusephraim@gmail.com>
2025-10-23 09:58:33 +0700

    Add files via upload

---
+++ KELAS-X-RPL/kelas/23-OKT-2025/register.php
@@ -0,0 +1,14 @@
+<h1>Register</h1>
+
+
+
+<form action="" method="post">
+<input type="text" name="nama" id="" placeholder="Nama">
+<br>
+<input type="email" name="email" id="" placeholder="Email">
+<br>
+<input type="password" name="password" id="" placeholder="Password">
+<br>
+<input type="submit" name="register" value="Register">
+<br>
+</form>
